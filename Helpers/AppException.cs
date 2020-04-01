26af3c3e01530ce13507520e629da2c759eb0e9d
Matthias Maag <maag.matthias@outlook.com>
2020-04-01 22:38:14 +0200

    project created

---
+++ Helpers/AppException.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Globalization;
+
+namespace SimpleApiAuthRegUser.Helpers
+{
+    // Custom exception class for throwing application specific exceptions ( e.g. validation)
+    // that can be caught and handled within the application
+    public class AppException : Exception
+    {
+        public AppException() : base(){}
+        
+        public AppException(string message) : base(message) {}
+
+        public AppException(string message, params object[] args)
+            : base(String.Format(CultureInfo.CurrentCulture, message, args))
+        {
+            
+        }
+    }
+}
