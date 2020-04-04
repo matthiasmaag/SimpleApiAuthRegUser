bc13d16af630fd26e5878e3b2e01805d34afb823
Matthias Maag <maag.matthias@outlook.com>
2020-04-04 21:28:30 +0200

    code formatting

--- Models/RegisterModel.cs
+++ Models/RegisterModel.cs
@@ -4,12 +4,16 @@ namespace SimpleApiAuthRegUser.Models
 {
     public class RegisterModel
     {
-        [Required] public string FirstName { get; set; }
+        [Required] 
+        public string FirstName { get; set; }
 
-        [Required] public string LastName { get; set; }
+        [Required] 
+        public string LastName { get; set; }
 
-        [Required] public string Username { get; set; }
+        [Required] 
+        public string Username { get; set; }
 
-        [Required] public string Password { get; set; }
+        [Required] 
+        public string Password { get; set; }
     }
 }
